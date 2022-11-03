a027edf92a2cda2ff1220e3b6c62bcfce01ad741
Saull Brandão <saull@outlook.com>
2022-11-03 10:31:53 -0300

    ngIf

---
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css'],
+})
+export class DiretivaNgifComponent implements OnInit {
+  cursos: string[] = ['Angular'];
+
+  mostrarCursos: boolean = false;
+
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
